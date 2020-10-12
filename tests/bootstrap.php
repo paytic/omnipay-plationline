dc886ca66f8e5911bacee20fbaf956b20e2f083f
Gabriel Solomon <hello@gabrielsolomon.ro>
2020-10-12 09:10:01 +0300

    update tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -9,6 +9,7 @@ require dirname(__DIR__) . '/vendor/autoload.php';
 if (file_exists(TEST_BASE_PATH . DIRECTORY_SEPARATOR . '.env')) {
     $dotenv = Dotenv\Dotenv::createImmutable(TEST_BASE_PATH);
     $dotenv->load();
-    putenv('PLATIONLINE_PUBLIC_KEY='.gzinflate(base64_decode(getenv('PLATIONLINE_PUBLIC_KEY'))));
-    putenv('PLATIONLINE_PRIVATE_KEY='.gzinflate(base64_decode(getenv('PLATIONLINE_PRIVATE_KEY'))));
 }
+
+putenv('PLATIONLINE_PUBLIC_KEY='.gzinflate(base64_decode(getenv('PLATIONLINE_PUBLIC_KEY'))));
+putenv('PLATIONLINE_PRIVATE_KEY='.gzinflate(base64_decode(getenv('PLATIONLINE_PRIVATE_KEY'))));
